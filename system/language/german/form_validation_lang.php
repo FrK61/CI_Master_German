18fc8381ab9a5d9412f57dea0d44bc54244971f1
FrK61 <oezcan.serkan@gmail.com>
2015-08-06 16:43:18 +0200

    CI_Master German Translation

---
+++ system/language/german/form_validation_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+
+$lang['required'] 			= "Das %s Feld ist Bedingung.";
+$lang['isset']				= "Das %s Feld muss einen Wert enthalten.";
+$lang['valid_email']		= "Das %s Feld muss eine gültige E-Mail-Adresse enthalten.";
+$lang['valid_emails'] 		= "Das %s Feld muss gültige E-Mail-Adressen enthalten.";
+$lang['valid_url'] 			= "Das %s Feld muss einen gültigen URL enthalten.";
+$lang['valid_ip'] 			= "Das %s Feld muss eine gültige IP enthalten.";
+$lang['min_length']			= "Das %s Feld muss mindestens %s Zeichen lang sein.";
+$lang['max_length']			= "Das %s Feld darf nicht %s Zeichen überschreiten.";
+$lang['exact_length']		= "Das %s Feld muss genau %s Zeichen lang sein.";
+$lang['alpha']				= "Das %s Feld darf nur alphabetische Zeichen enthalten.";
+$lang['alpha_numeric']		= "Das %s Feld darf nur alphanumerische Zeichen enthalten.";
+$lang['alpha_dash']			= "Das %s Feld darf nur alphanumerische Zeichen, Unterstriche und Bindestriche enthalten.";
+$lang['numeric']			= "Das %s Feld darf nur Zahlen enthalten.";
+$lang['is_numeric']			= "Das %s Feld darf nur numerische Zeichen enthalten.";
+$lang['integer']			= "Das %s Feld muss einen Integer-Wert enthalten.";
+$lang['regex_match']		= "The %s field is not in the correct format.";
+$lang['matches']			= "Das %s Feld stimmt nicht mit Feld %s überein.";
+$lang['is_unique'] 			= "The %s field must contain a unique value.";
+$lang['is_natural']			= "Das %s Feld darf nur positive Zahlenwerte enthalten.";
+$lang['is_natural_no_zero']	= "Das %s Feld muss einen Wert größer als Null enthalten.";
+$lang['decimal']			= "The %s field must contain a decimal number.";
+$lang['less_than']			= "The %s field must contain a number less than %s.";
+$lang['greater_than']		= "The %s field must contain a number greater than %s.";
+
+
+/* End of file form_validation_lang.php */
+/* Location: ./system/language/english/form_validation_lang.php */
